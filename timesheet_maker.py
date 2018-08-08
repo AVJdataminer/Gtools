66013471743e984f97a3699711ca802b9044eaef
AVJdataminer <aiden.dataminer@gmail.com>
2018-08-08 15:52:01 -0600

    Updated prefix and description

--- timesheet_maker.py
+++ timesheet_maker.py
@@ -1,20 +1,8 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# This was created from the basic quickstart.py for google calendar API.
 
 # Change the flow = to your personal google calendar api credentials stored on your local machine.
 """
-Shows basic usage of the Google Calendar API.This was created from the basic quickstart.py for google calendar API.
+Pulls the 30 events in your calendar after the set local datetime
 """
 from __future__ import print_function
 from apiclient.discovery import build
@@ -71,4 +59,5 @@ for event in events:
     df=pd.DataFrame(d, columns=('Date', 'Item','Allocate Towards','Rate','Qty in Hours','Total'))
     df.to_csv('gtime_sheet.csv')
     #np.savetxt("gtime_sheet.csv", c, delimiter=",")
+    print(df)
 # [END]
